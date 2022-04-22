25f2cf4aad35edf95bd3c26a9a8065970d7b4b09
plump_albert <plumpalbert@gmail.com>
2022-04-22 17:04:36 +0300

    Fixed JSON format

--- generate.sh
+++ generate.sh
@@ -7,5 +7,5 @@ ROOT="$(dirname "$0")"
 for FILE in $XCOLORS_ROOT/*; do
 	awk \
 		'BEGIN{print "{"}$1 == "#define" {print "\""$2"\":\""$3"\","} END{print "}"}' \
-		"$FILE" >"$ROOT/colorschemes/$(basename "$FILE")"
+		"$FILE" | tac | sed '2s/,$//' | tac >"$ROOT/colorschemes/$(basename "$FILE")"
 done
